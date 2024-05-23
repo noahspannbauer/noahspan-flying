294ac4e06118955ce4ad4ef9aab8727328d4b5e1
noahspannbauer <noahspannbauer@icloud.com>
2024-05-22 21:12:17 -0500

    Setup Playwright (#14)
    * setting up playwright
    
    * renaming client to app
    
    * updating node version
    
    * updating node version
    
    * fixing husky
    
    * fixing husky
    
    * fixing husky
    
    * fixing husky

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,16 +4,15 @@ import Logbook from './components/logbook/Logbook';
 import Checklists from './components/checklists/Checklists';
 import Pilots from './components/pilots/Pilots';
 
-
 const App: React.FC<unknown> = () => {
   return (
     <Routes>
-      <Route path='/' element={<Flights />} />
-      <Route path='logbook' element={<Logbook />} />
-      <Route path='checklists' element={<Checklists />} />
-      <Route path='pilots' element={<Pilots />} />
+      <Route path="/" element={<Flights />} />
+      <Route path="logbook" element={<Logbook />} />
+      <Route path="checklists" element={<Checklists />} />
+      <Route path="pilots" element={<Pilots />} />
     </Routes>
   );
-}
+};
 
 export default App;
---
+++ tests/e2e/support/config.ts
@@ -0,0 +1,30 @@
+import { LaunchOptions, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+const browserOptions: LaunchOptions = {
+  headless: Boolean(process.env.HEADLESS) || false,
+  timeout: Number(process.env.TIMEOUT) || 60000,
+  args: [
+    '--use-fake-ui-for-media-stream',
+    '--use-fake-device-for-media-stream'
+  ],
+  firefoxUserPrefs: {
+    'media.navigator.streams.fake': true,
+    'media.navigator.permission.disable': true
+  },
+  slowMo: Number(process.env.SLO_MO) || 0
+};
+const desktopChrome = devices['Desktop Chrome'];
+
+export const config = {
+  browser: process.env.BROWSER || 'chromium',
+  browserOptions,
+  baseUrl: process.env.BASE_URL || 'http://localhost:3000',
+  username: process.env.TEST_USERNAME,
+  password: process.env.TEST_PASSWORD,
+  viewportHeight: process.env.VIEWPORT_HEIGHT || desktopChrome.viewport.height,
+  viewportWidth: process.env.VIEWPORT_WIDTH || desktopChrome.viewport.width,
+  userAgent: process.env.USER_AGENT || desktopChrome.userAgent
+};
