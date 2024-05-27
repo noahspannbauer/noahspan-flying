80a86a281c8570ca4277aaa65156aa8f5bee6ee8
Noah Spannbauer <nspannbauer@sensonix.com>
2024-05-26 19:28:28 -0500

    Adding app config module

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { AzAppConfigService } from '@noahspan/noahspan-modules';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly azAppConfigService: AzAppConfigService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('featureFlag')
+  getFeatureFlag(key: string, label: string): string {
+    const featureFlag = this.azAppConfigService.getFeatureFlag(key, label);
+    console.log(process.env.AZ_APP_CONFIG_CONNECTION_STRING);
+    return 'blah';
   }
 }
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AzAppConfigModule } from '@noahspan/noahspan-modules';
 
 @Module({
-  imports: [],
+  imports: [
+    AzAppConfigModule.register({
+      connectionString: process.env.AZ_APP_CONFIG_CONNECTION_STRING
+    })
+  ],
   controllers: [AppController],
   providers: [AppService]
 })
