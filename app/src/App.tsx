34aac62ec8816003d19ff44f2cb8b0002cd9b381
Noah Spannbauer <noahspannbauer@icloud.com>
2024-12-01 19:29:02 -0600

    api no longer fails when no pilots or logbook entries

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -12,36 +12,36 @@ const App: React.FC<unknown> = () => {
   const httpClient: AxiosInstance = useHttpClient();
   const appContext = useAppContext();
 
-  // useEffect(() => {
-  //   const getFeatureFlags = async () => {
-  //     try {
-  //       const featureFlagKeys: string = 'flying-pilots';
-  //       const response: AxiosResponse = await httpClient.get(
-  //         `api/featureFlags?keys=${featureFlagKeys}&label=${import.meta.env.MODE}`
-  //       );
-  //       const featureFlags: { key: string; enabled: boolean }[] = response.data;
+  useEffect(() => {
+    const getFeatureFlags = async () => {
+      try {
+        const featureFlagKeys: string = 'flying-pilots';
+        const response: AxiosResponse = await httpClient.get(
+          `api/featureFlags?keys=${featureFlagKeys}&label=${import.meta.env.MODE}`
+        );
+        const featureFlags: { key: string; enabled: boolean }[] = response.data;
 
-  //       if (featureFlags.length > 0) {
-  //         appContext.dispatch({
-  //           type: 'SET_FEATURE_FLAGS',
-  //           payload: featureFlags
-  //         });
-  //       }
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+        if (featureFlags.length > 0) {
+          appContext.dispatch({
+            type: 'SET_FEATURE_FLAGS',
+            payload: featureFlags
+          });
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-  //   getFeatureFlags();
-  // }, []);
+    getFeatureFlags();
+  }, []);
 
   return (
     <div>
       <SiteNav />
       <Routes>
-        {/* {useFeatureFlag('flying-pilots')?.enabled && ( */}
-        <Route path="/pilots" element={<Pilots />} />
-        {/* )} */}
+        {useFeatureFlag('flying-pilots')?.enabled && (
+          <Route path="/pilots" element={<Pilots />} />
+        )}
         <Route path="/" element={<Logbook />} />
       </Routes>
     </div>
