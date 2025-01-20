5f9a963938a1c24311ddc93ba21639a20e88eab2
Noah Spannbauer <noahspannbauer@icloud.com>
2025-01-19 20:28:02 -0600

    Refactoring

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -18,31 +18,31 @@ const msalConfig = {
     //     cacheLocation: "sessionStorage", // This configures where your cache will be stored
     //     storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
     // },
-    system: {	
-        loggerOptions: {	
-            loggerCallback: (level: any, message: any, containsPii: any) => {	
-                if (containsPii) {		
-                    return;		
-                }		
-                switch (level) {
-                    case LogLevel.Error:
-                        console.error(message);
-                        return;
-                    case LogLevel.Info:
-                        console.info(message);
-                        return;
-                    case LogLevel.Verbose:
-                        console.debug(message);
-                        return;
-                    case LogLevel.Warning:
-                        console.warn(message);
-                        return;
-                    default:
-                        return;
-                }	
-            }	
-        }	
-    }
+    // system: {	
+    //     loggerOptions: {	
+    //         loggerCallback: (level: any, message: any, containsPii: any) => {	
+    //             if (containsPii) {		
+    //                 return;		
+    //             }		
+    //             switch (level) {
+    //                 case LogLevel.Error:
+    //                     console.error(message);
+    //                     return;
+    //                 case LogLevel.Info:
+    //                     console.info(message);
+    //                     return;
+    //                 case LogLevel.Verbose:
+    //                     console.debug(message);
+    //                     return;
+    //                 case LogLevel.Warning:
+    //                     console.warn(message);
+    //                     return;
+    //                 default:
+    //                     return;
+    //             }	
+    //         }	
+    //     }	
+    // }
 };
 
 const msalInstance = new PublicClientApplication(msalConfig)
