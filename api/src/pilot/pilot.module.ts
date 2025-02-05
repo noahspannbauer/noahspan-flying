01127e6f186344c9b83a9e2e97c6a306ab7c0045
Noah Spannbauer <noahspannbauer@icloud.com>
2025-02-04 18:32:07 -0600

    adding pilot delete

--- api/src/pilot/pilot.module.ts
+++ api/src/pilot/pilot.module.ts
@@ -4,6 +4,7 @@ import { PilotService } from './pilot.service';
 import { AzureTableStorageModule } from '@noahspan/azure-database';
 import { Pilot } from './pilot.entity';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Log } from 'src/log/log.entity';
 
 @Module({
   imports: [
@@ -16,10 +17,23 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       },
       inject: [ConfigService]
     }),
+    AzureTableStorageModule.forFeature(Log, {
+      createTableIfNotExists: false,
+      table: 'logs'
+    }),
+    AzureTableStorageModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          connectionString: configService.get<string>('azureStorageConnectionString')
+        };
+      },
+      inject: [ConfigService]
+    }),
     AzureTableStorageModule.forFeature(Pilot, {
       createTableIfNotExists: false,
       table: 'pilots'
-    }),
+    })
   ],
   controllers: [PilotController],
   providers: [PilotService]
