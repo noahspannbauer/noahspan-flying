b74e41964619020ee74398b8815d6c2c495c65f1
Noah Spannbauer <noahspannbauer@icloud.com>
2025-03-23 11:44:51 -0500

    adding flights page

--- app/src/components/flights/Flights.tsx
+++ app/src/components/flights/Flights.tsx
@@ -1,12 +1,12 @@
-import { Box, Container } from "@noahspan/noahspan-components";
+import { Box, Container, Grid, Spinner } from "@noahspan/noahspan-components";
 import LogbookCard from "../logbookCard/LogbookCard";
 import { useEffect, useState } from "react";
 import { useLogs } from "../../hooks/logs/UseLogs";
 import { ILogbookEntry } from "../logbook/ILogbookEntry";
 
 const Flights = () => {
-  const [flights, setFlights] = useState<ILogbookEntry[]>([])
-  const { logs } = useLogs();
+  const [flights, setFlights] = useState<ILogbookEntry[]>([]);
+  const { logs, isLoading } = useLogs();
 
   useEffect(() => {
     const flights: ILogbookEntry[] | undefined = logs?.filter((log: ILogbookEntry) => {
@@ -23,7 +23,23 @@ const Flights = () => {
   return (
     <Container>
       <Box sx={{ margin: '20px' }}>
-        <LogbookCard logs={flights} mode='flights' />
+        <Grid container spacing={2}>
+          {isLoading &&
+            <>
+              <Grid display="flex" justifyContent="center" size={12}>
+                <Spinner />
+              </Grid>
+              <Grid display="flex" justifyContent="center" size={12}>
+                Loading...
+              </Grid>
+            </>
+          }
+          {!isLoading &&
+            <Grid size={12}>
+              <LogbookCard logs={flights} mode='flights' />
+            </Grid>
+          }
+        </Grid>
       </Box>
     </Container>
   )
